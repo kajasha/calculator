c7377ef5402ba33bee8801ee926f0c55e188e213
Karola <yumi.chan@interia.pl>
2016-07-25 11:39:03 +0200

    Start projektu!

---
+++ src/it/morfoza/Main.java
@@ -0,0 +1,13 @@
+package it.morfoza;
+
+/**
+ * Created by HP 15 on 2016-07-25.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Witaj!");
+    }
+}
