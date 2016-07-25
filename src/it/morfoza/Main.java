02d6e908e35d308005eb96c26b0c0bc322a677e3
Karola <yumi.chan@interia.pl>
2016-07-25 11:45:07 +0200

    English version

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -8,6 +8,6 @@ package it.morfoza;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Witajcie petardy i ziomeczki!");
+        System.out.println("Hola!");
     }
 }
