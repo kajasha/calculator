c46410633a310b1cf2387b77c87dab71e220a917
Karola <yumi.chan@interia.pl>
2016-07-26 15:55:04 +0200

    pom.xml.change

--- src/main/java/it/morfoza/CalculatorWeb.java
+++ src/main/java/it/morfoza/CalculatorWeb.java
@@ -29,6 +29,7 @@ public class CalculatorWeb {
                             + "</html>";
 
                 });
+
         Spark.get(
                 "/contact",
                 ((request, response) -> {
